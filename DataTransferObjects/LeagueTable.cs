61edf29a9d63e9ceb18ae097e72b7a671d71a125
git <tobi.eriksson@gmail.com>
2016-04-06 20:26:48 +0200

    Ändringar i klass för Ligatabell

--- DataTransferObjects/LeagueTable.cs
+++ DataTransferObjects/LeagueTable.cs
@@ -8,11 +8,33 @@ namespace DataTransferObjects
 {
     public class LeagueTable
     {
-        public LeagueTable(Team[] teams)
+        private List<LeagueTableRow> _rows;
+        public LeagueTable(List<LeagueTableRow> rows)
         {
-            Teams = teams;
+            _rows.AddRange(rows);
         }
-        Team[] Teams;
 
+        public LeagueTableRow GetTeam(String Name)
+        {
+            var row = from r in _rows
+            where r.Team.Name == Name
+            select r;
+            return row.FirstOrDefault();
+        }
+
+        public class LeagueTableRow
+        {
+            public int Place { get; set; }
+            public Team Team { get; set; }
+            public short GamesPlayed { get; set; }
+            public short Wins { get; set; }
+            public short Draw { get; set; }
+            public short Losses { get; set; }
+            public short GoalsFor { get; set; }
+            public short GoalsAgainst { get; set; }
+            public short Points { get; set; }
+        }
     }
+
+    
 }
